19b207d8c37cccbc735be397be01e247ceb0034d
Louis <emyhoang8899@gmail.com>
2020-02-25 18:58:31 -0800

    create basic event handler on buttons
    - action = console.log currently
    - change function app to class app in app.tsx
    - implement arrow function handlers for props in app.tsx
    - reference functions in confirm component

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import Confirm from './confirm';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-      <Confirm
-        title='React and TypeScript'
-        content='Are you sure you want to learn React and Typescript?'
-        cancelCaption='No way'
-        okCaption='Yes please!'
-      />
-    </div>
-  );
+class App extends React.Component {
+  private handleCancelConfirmClick = () => {
+    console.log('Cancel clicked');
+  };
+
+  private handleOkConfirmClick = () => {
+    console.log('Ok clicked');
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+            Learn React
+          </a>
+        </header>
+        <Confirm
+          title='React and TypeScript'
+          content='Are you sure you want to learn React and Typescript?'
+          cancelCaption='No way'
+          okCaption='Yes please!'
+          onCancelClick={this.handleCancelConfirmClick}
+          onOkClick={this.handleOkConfirmClick}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
